d5f40fd824f4691ee6acf6cfdfdafd3e8eec60cc
Charles G <charles.grigny@ipsa.fr>
2022-06-17 15:14:26 +0200

    Public buy and sell trading bot

--- snipingsellboto.js
+++ snipingsellboto.js
@@ -1,5 +1,5 @@
 const ethers = require('ethers');
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 const { Contract } = require('ethers');
 const secret = require('./secret')
 // Installer ethers et prompt (npm install)
@@ -234,97 +234,97 @@ abii = [
     }
 ]
 
-const gwei = '5'     
-const slippage = 0   
+const gwei = '5'
+const slippage = 0
 
 
 const gasPrice = ethers.utils.parseUnits(gwei, 'gwei');
 const gas = {
-  gasPrice: gasPrice,
-  gasLimit: 1000000
+    gasPrice: gasPrice,
+    gasLimit: 1000000
 }
 
 
 const options = {
     address: myAddress,
-    provider: BSCprovider, 
-  }
+    provider: BSCprovider,
+}
 
-const getBalance = async (options,tokenAddress) => {
+const getBalance = async (options, tokenAddress) => {
     const contract = new Contract(tokenAddress, abii, options.provider);
     const balance = await contract.balanceOf(options.address);
     b = balance._hex
     return b;
-  };
+};
 
 
-  const addresses = {
+const addresses = {
     //WBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",   // Must have BNB
     WBNB: "0xe9e7cea3dedca5984780bafc599bd69add087d56", // If BUSD on the pair
     //WBNB:"0x55d398326f99059ff775485246999027b3197955", // If USDT
     router: "0x10ed43c718714eb63d5aa57b78b54704e256024e", // Router pancakeswap
-    target:  myAddress                                    
-    }
+    target: myAddress
+}
 
-const account = new ethers.Wallet(privateKey, BSCprovider); 
-const router = new ethers.Contract( 
-  addresses.router,
-  [
-    'function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
-  ],
-  account
-);
-
-const router2 = new ethers.Contract( 
+const account = new ethers.Wallet(privateKey, BSCprovider);
+const router = new ethers.Contract(
     addresses.router,
     [
-      'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
-  
+        'function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
     ],
     account
-  );
+);
+
+const router2 = new ethers.Contract(
+    addresses.router,
+    [
+        'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+
+    ],
+    account
+);
 
 
 const snipe = async (token) => {
     BNBAmount = await getBalance(options, token);
     console.log(BNBAmount);
-    if(addresses.WBNB==="0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"){
-    const tx = await router.swapExactTokensForETH(
-    BNBAmount,
-    slippage, 
-    [token,addresses.WBNB],
-    addresses.target,
-    Math.floor(Date.now() / 1000) + 60 * 20, 
-    {
-        ...gas,
-    });
-    console.log(`Swapping BNB for tokens...`);
-    const receipt = await tx.wait();
-    console.log(`Transaction hash: ${receipt.transactionHash}`);    
-    }else{
-    console.log("noooo");
-    console.log(BNBAmount);
-    const tx = await router2.swapExactTokensForTokens(
-    BNBAmount,
-    slippage, // Degen ape don't give a fuxk about slippage
-    [token,addresses.WBNB],
-    addresses.target,
-    Math.floor(Date.now() / 1000) + 60 * 20, // 10 minutes from now
-    {
-        ...gas
+    if (addresses.WBNB === "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c") {
+        const tx = await router.swapExactTokensForETH(
+            BNBAmount,
+            slippage,
+            [token, addresses.WBNB],
+            addresses.target,
+            Math.floor(Date.now() / 1000) + 60 * 20,
+            {
+                ...gas,
+            });
+        console.log(`Swapping BNB for tokens...`);
+        const receipt = await tx.wait();
+        console.log(`Transaction hash: ${receipt.transactionHash}`);
+    } else {
+        console.log("noooo");
+        console.log(BNBAmount);
+        const tx = await router2.swapExactTokensForTokens(
+            BNBAmount,
+            slippage, // Degen ape don't give a fuxk about slippage
+            [token, addresses.WBNB],
+            addresses.target,
+            Math.floor(Date.now() / 1000) + 60 * 20, // 10 minutes from now
+            {
+                ...gas
+            }
+        );
+        console.log(`Swapping BNB for tokens...`);
+        const receipt = await tx.wait();
+        console.log(`Transaction hash: ${receipt.transactionHash}`);
     }
-    );
-    console.log(`Swapping BNB for tokens...`);
-    const receipt = await tx.wait();
-    console.log(`Transaction hash: ${receipt.transactionHash}`);
-    }
 
 
- }
+}
 
 
-const token = prompt('Le token que tu veux vendre chef : ');
+const token = prompt('What do you want to sell ? ');
 
 (async () => {
-  await snipe(token);
+    await snipe(token);
 })();
